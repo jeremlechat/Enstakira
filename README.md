44799a12118b4b08c038b7c45bddd51e0daa14e9
Bespocke <leo.tuan@laposte.net>
2025-02-11 20:51:20 +0100

    init commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# ENST'AKIRA - Site du BDE
+
+Bienvenue sur le site officiel de **ENST'AKIRA**, le Bureau des Élèves (BDE) de l'ENSTA Bretagne !
+
+## Auteurs
+
+- **Nils Xhoffray** : Développeur principal et créateur du site.
+
+## Licence
+
+Ce projet est sous **l'UNLICENSE**. Il est dans le domaine public et vous pouvez l'utiliser, le modifier et le redistribuer à votre guise, même à des fins commerciales, sans aucune restriction.
+
+Pour plus d'informations, consultez [unlicense.org](http://unlicense.org/).
---
+++ script.js
@@ -0,0 +1,28 @@
+document.addEventListener('click', triggerEffect);
+document.addEventListener('keydown', triggerEffect);
+
+function triggerEffect() {
+    const background = document.getElementById('background');
+    const shinyImage = document.getElementById('shiny-image');
+    const linkButtons = document.getElementById('link-buttons');
+
+    // Appliquer l'effet de flou au fond
+    background.classList.add('blur-md');
+
+    // Appliquer l'animation de translation et agrandissement à l'image
+    shinyImage.style.transform = `translateY(-${window.innerHeight * 0.3}px)`; // Décalage vertical
+    shinyImage.classList.remove('opacity-0');
+    shinyImage.classList.add('opacity-100', 'image-appear'); // Ajouter l'animation de translation et agrandissement
+
+    // Afficher les boutons de lien avec animation
+    linkButtons.classList.remove('opacity-0');
+    linkButtons.classList.add('bottom-1/4', 'opacity-100');
+
+    // Ajouter l'animation d'apparition des boutons 
+    const buttons = Array.from(linkButtons.children);
+    buttons.forEach((button, index) => {
+        setTimeout(() => {
+            button.classList.add('button-appear'); // Ajouter l'animation d'apparition
+        }, index * 150); // Décalage de l'apparition des boutons
+    });
+}
